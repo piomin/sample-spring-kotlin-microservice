66efc422a4caad366917e658cf3be671e5eaeb56
Piotr Mińkowski <piotr_minkowski@wp.pl>
2020-03-10 00:43:56 +0100

    adding Liquibase and Rabbit auto-creation

--- src/main/kotlin/pl/piomin/services/model/Person.kt
+++ src/main/kotlin/pl/piomin/services/model/Person.kt
@@ -1,7 +1,9 @@
 package pl.piomin.services.model
 
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.Id
 
 @Entity
-data class Person(@Id var id: Int?, var name: String, var age: Int, var gender: Gender)
+data class Person(@Id var id: Int?, var name: String, var age: Int, @Enumerated(EnumType.ORDINAL) var gender: Gender)
