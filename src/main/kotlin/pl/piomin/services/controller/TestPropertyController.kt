c340eb7b4402e94970957baadca38b3fe3aefd70
minkowp <piotr.minkowski@play.pl>
2020-03-09 15:29:33 +0100

    adding configmap

---
+++ src/main/kotlin/pl/piomin/services/controller/TestPropertyController.kt
@@ -0,0 +1,9 @@
+package pl.piomin.services.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/properties")
+class TestPropertyController {
+}
