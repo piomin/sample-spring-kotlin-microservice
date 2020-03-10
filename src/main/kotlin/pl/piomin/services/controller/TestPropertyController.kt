e287191c2ce67ad1f533b9dfccac35ba04e0d742
Piotr Mińkowski <piotr_minkowski@wp.pl>
2020-03-10 12:44:53 +0100

    final changes

--- src/main/kotlin/pl/piomin/services/controller/TestPropertyController.kt
+++ src/main/kotlin/pl/piomin/services/controller/TestPropertyController.kt
@@ -1,9 +1,15 @@
 package pl.piomin.services.controller
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/properties")
-class TestPropertyController {
+class TestPropertyController(@Value("\${property1}") val property1: String) {
+
+    @GetMapping
+    fun printProperty1(): String  = property1
+
 }
