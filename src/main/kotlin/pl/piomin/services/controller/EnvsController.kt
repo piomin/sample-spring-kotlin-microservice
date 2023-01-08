217d85659bb6da097bbe1664faf438bd4878770a
piomin <piotr.minkowski@gmail.com>
2023-01-08 01:22:09 +0100

    add renovate + circleci + envs

---
+++ src/main/kotlin/pl/piomin/services/controller/EnvsController.kt
@@ -0,0 +1,17 @@
+package pl.piomin.services.controller
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/envs")
+class EnvsController {
+
+    @Value("\${PASS}")
+    lateinit var password: String
+
+    @GetMapping
+    fun info(): String = password
+}
