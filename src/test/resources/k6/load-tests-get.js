fe38fb6fbe2fb68e2cf0549a119472f332f410f8
piomin <piotr.minkowski@gmail.com>
2023-08-25 12:48:41 +0200

    adding k6 tests

---
+++ src/test/resources/k6/load-tests-get.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+export default function () {
+  const id = randomIntBetween(1, 2000);
+  const res = http.get(`${__ENV.PERSONS_URI}/persons/${id}`);
+  check(res, {
+    'is status 200': (res) => res.status === 200,
+    'body size is > 0': (r) => r.body.length > 0,
+  });
+}
