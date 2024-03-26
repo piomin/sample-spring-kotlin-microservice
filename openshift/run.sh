bee7dd0ffbea388da5d4631a899fbe4173ea52a6
piomin <piotr.minkowski@gmail.com>
2024-03-26 17:18:23 +0100

    add runpipeline scripts

---
+++ openshift/run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+for OUTPUT in $(git branch -r)
+do
+  branch=$(echo $OUTPUT | sed -e "s/^origin\///")
+  if [[ $branch == feature* ]]
+  then
+    echo "Running the pipeline: branch="$branch
+    tkn pipeline start sample-pipeline -p branch=$branch -p namespace=app-$branch -w name=source-dir,volumeClaimTemplateFile=pvc.yaml
+  fi
+done
