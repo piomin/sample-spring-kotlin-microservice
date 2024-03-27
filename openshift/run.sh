6b7acd6617f296b88ce09fed11f3e7c1085885d6
piomin <piotr.minkowski@gmail.com>
2024-03-27 08:56:22 +0100

    add runpipeline scripts

--- openshift/run.sh
+++ openshift/run.sh
@@ -6,6 +6,6 @@ do
   if [[ $branch == feature* ]]
   then
     echo "Running the pipeline: branch="$branch
-    tkn pipeline start sample-pipeline -p branch=$branch -p namespace=app-$branch -w name=source-dir,volumeClaimTemplateFile=pvc.yaml
+    tkn pipeline start sample-pipeline -p branch=$branch -p namespace=app-$branch -w name=source-dir,volumeClaimTemplateFile=pvc.yaml >/dev/null
   fi
 done
